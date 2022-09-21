f434e72826c2ea17d490c1900d3927d78cd40c45
Ryan Gibb <ryan@gibbr.org>
2022-09-21 15:37:34 +0200

    nix build support

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    hillingar.url = "github:RyanGibb/hillingar";
+
+    # use different repositories to those pinned in hillingar
+    hillingar.inputs.opam-repository.follows = "opam-repository";
+    hillingar.inputs.opam-overlays.follows = "opam-overlays";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-overlays = {
+      url = "github:dune-universe/opam-overlays";
+      flake = false;
+    };
+
+    # make hillingar's nixpkgs follow this flake
+    # useful if pinning nixos system nixpkgs with
+    #   `nix.registry.nixpkgs.flake = nixpkgs;`
+    hillingar.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, hillingar, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        packages = let
+          mirage-nix = (hillingar.lib.${system});
+          inherit (mirage-nix) mkUnikernelPackages;
+        in
+          mkUnikernelPackages {
+            unikernelName = "www";
+            depexts = with pkgs; [ gmp ];
+            mirageDir = "mirage";
+          } self;
+
+        defaultPackage = self.packages.${system}.unix;
+
+        devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
